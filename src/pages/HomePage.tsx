a2d7d207669f2d1e493b71d8e53391435b94a4b8
KoSi <otpog23@gmail.com>
2026-02-16 15:43:21 +0000

    Homepage - Post styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import '../styles/HomePage.css'
 
 export function HomePage() {
+
+    const [more, setMore] = useState<Boolean>(false);
 
     return (
         <div className='homepage'>
@@ -12,17 +15,19 @@ export function HomePage() {
 
             <section className='posts'>
                 <div className="post">
-                    <div className="asr">
-                        ASR:
-                    </div>
-
                     <div className="details">
-                        <div className="date-time">
-                            16/02/2026 : 11:45
+                        <div className='asr-date'>
+                            <div className="asr">
+                                ASR:
+                            </div>
+
+                            <div className="date">
+                                16/02/2026 : 11:45
+                            </div>
                         </div>
 
                         <div className="fav">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M10.4107 19.9677C7.58942 17.858 2 13.0348 2 8.69444C2 5.82563 4.10526 3.5 7 3.5C8.5 3.5 10 4 12 6C14 4 15.5 3.5 17 3.5C19.8947 3.5 22 5.82563 22 8.69444C22 13.0348 16.4106 17.858 13.5893 19.9677C12.6399 20.6776 11.3601 20.6776 10.4107 19.9677Z" />
                             </svg>
                         </div>
@@ -35,7 +40,7 @@ export function HomePage() {
                         </div>
 
                         <div className="delete">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round">
                                 <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" />
                                 <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" />
                                 <path d="M9.5 16.5L9.5 10.5" />
@@ -45,12 +50,12 @@ export function HomePage() {
                     </div>
 
                     <div className="caption">
-                        <span className='text'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quo saepe eius, fugiat temporibus architecto quaerat unde sunt animi vero debitis repudiandae ducimus molestias tempora distinctio sit expedita quos? Quod.
+                        <span className='text' id={`${more == false ? 'text' : ''}`}>
+                            - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eveniet, omnis possimus repudiandae pariatur cupiditate quis, sequi laborum commodi veritatis fugiat magni rerum laudantium molestias tenetur quia enim? Autem, delectus.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quo saepe eius, fugiat temporibus architecto quaerat unde sunt animi vero debitis repudiandae ducimus molestias tempora distinctio sit expedita quos? Quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad nam beatae iste id officia maxime. Laboriosam ut tempora aliquid non. Dolorem quasi, ducimus qui ipsam facere esse quae non?
                         </span>
 
-                        <span className='more'>
-                            ...more
+                        <span className='more' onClick={() => setMore(m => !m)}>
+                            {more == false ? 'more' : 'less'}
                         </span>
                     </div>
 
@@ -58,7 +63,6 @@ export function HomePage() {
                 </div>
 
                 <div className="post">
-
                     <div className="details">
                         <div className='asr-date'>
                             <div className="asr">
@@ -84,7 +88,7 @@ export function HomePage() {
                         </div>
 
                         <div className="delete">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round">
                                 <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" />
                                 <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" />
                                 <path d="M9.5 16.5L9.5 10.5" />

116999ca7be4819ce5a72cef8153e9116b1658bc
KoSi <otpog23@gmail.com>
2026-02-16 11:11:47 +0000

    Added: [Sidebar]

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./Header";
+import '../styles/MainPage.css'
 
 
 export function MainPage() {
 
     return (
-        <>
+        <div>
             <Header/>
-        </>
+        </div>
     )
 }
